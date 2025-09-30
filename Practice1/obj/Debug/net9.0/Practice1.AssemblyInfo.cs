bc65dd683f66553c9c8b0b1d019b468f142ac608
M1kuAr1us <ivan.zavivan@gmail.com>
2025-09-30 15:06:54 +0300

    Edit UML Diagrams

--- Practice1/obj/Debug/net9.0/Practice1.AssemblyInfo.cs
+++ Practice1/obj/Debug/net9.0/Practice1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practice1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+288c749a3f67540e8859a62c7590619408ab77ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+667cb4cd7dcc4c9733dd607f1671326f30c98054")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practice1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practice1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
