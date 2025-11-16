602eaeea00d2c30e032e8e5cb3cae38518e0e341
IvanZavaliy <ivan.zavivan@gmail.com>
2025-11-17 00:51:00 +0200

    Complete Compendium Diagrams And Add C4 Diagrams

--- Practice1/obj/Debug/net9.0/Practice1.AssemblyInfo.cs
+++ Practice1/obj/Debug/net9.0/Practice1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practice1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+667cb4cd7dcc4c9733dd607f1671326f30c98054")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b102ff1e6446d6e54bacf1d986c01cc68b9cae4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practice1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practice1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
